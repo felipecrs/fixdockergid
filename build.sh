3eb3ea84fdb64da92e5a9543e6641bf1d2fd9be2
Felipe Santos <felipecassiors@gmail.com>
2024-03-06 12:26:58 -0300

    Improve build script

--- build.sh
+++ build.sh
@@ -4,24 +4,30 @@ set -euxo pipefail
 
 rm -rf dist
 
+docker run --privileged --rm --pull=always tonistiigi/binfmt --install arm64
+
+export BUILDX_BUILDER="fixdockergid"
+
+docker buildx create --name "${BUILDX_BUILDER}"
+trap 'docker buildx rm --force "${BUILDX_BUILDER}"' EXIT
+
 docker buildx build --target=dist --platform=amd64 -o . .
 docker buildx build --target=dist --platform=arm64 -o . .
 
+docker buildx build . --platform=linux/amd64,linux/arm64
+
 set +x
 
-echo "Release on GitHub with:" >&2
-# shellcheck disable=SC2016
-echo '  gh release create v${VERSION?} --title v${VERSION} dist/*' >&2
+if [[ -z "${VERSION:-}" ]]; then
+  echo "Set VERSION if you want to publish" >&2
+  exit 1
+fi
 
-echo "Push to Docker Hub with:" >&2
-# shellcheck disable=SC2016
-cat <<'EOF' >&2
-  docker run --privileged --rm --pull=always tonistiigi/binfmt --install arm64 \
-  && docker buildx rm --force multi-arch-builder \
-  && docker buildx create --name multi-arch-builder \
-  && docker buildx build . --builder multi-arch-builder --pull \
-    --tag felipecrs/fixdockergid:latest \
-    --tag felipecrs/fixdockergid:${VERSION?} \
-    --platform=linux/amd64,linux/arm64  \
-    --push
-EOF
+set -x
+
+gh release create "v${VERSION}" --title "v${VERSION}" dist/*
+
+docker buildx build . --platform=linux/amd64,linux/arm64 \
+  --tag=felipecrs/fixdockergid:latest \
+  --tag="felipecrs/fixdockergid:${VERSION}" \
+  --push
